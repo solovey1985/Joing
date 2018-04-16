14297a7fa23f5790e2761ac8273fc0b047bd0951
SOFTINVEST\Andrii.Solovienko <andrii.solovienko@uapay.ua>
2018-04-16 19:00:48 +0300

    [Api] ASP.NET Core WebApi Project

---
+++ Joing.Api/Yalldoo.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yalldoo.Api.Controllers
+{
+    public class HomeController: Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "val1", "val2" };
+        }
+    }
+}
